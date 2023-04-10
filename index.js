1dc669fafbbd1c1cdab3d3fb711e9ff65c39f587
cynthia <cynthiakagz@gmail.com>
2023-04-10 13:33:40 +0300

    Assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+document.querySelector('id')
+     const element= document.getElementById('main');
+     element.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id','victory')
+ newHeader.textContent = "YOUR-NAME is the champion";
+ document.body.appendChild(newHeader);
